7b2f05985faf293961d744e689f229e7e984194d
Kyle Cackett <Kyle.Cackett@gmail.com>
2012-06-22 22:29:22 -0400

    Added base for media page

---
+++ media.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<?php require "resources/constants.php";?>
+<html>
+	<head>
+		<title>Brown Slackline | Links</title>
+		<?php require $frameworks;?>
+		<script type="text/javascript">
+			var active = "#media";
+		</script>
+		<?php require $resources;?>
+	</head>
+	<body>
+		<?php require $navbar;?>
+		<div id="background-links" class="background">
+			<img id="backdrop-links" class="backdrop pull-left" src="assets/bgLinks.png"/>
+			<div class="container">
+				<h1 class="hero-font pagename pad-bottom">Media</h1>
+				<div class="span8 justify">
+					
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
